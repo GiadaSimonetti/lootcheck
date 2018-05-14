d47430a0d98eeec89aa19f033352f82cdec97d01
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-14 14:53:59 +0100

    Add App.js file + Babel configuration

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Enzyme from 'enzyme'
 import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 // import App from './App'
-import App from './src/components/App';
+import App from '../src/components/App';
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe ('App component', () => {
+
   const app = shallow(<App />);
 
   it('renders properly', () => {
