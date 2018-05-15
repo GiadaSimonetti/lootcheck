9640906fcdaa0f2a225a5b33c308a4ae815d8af4
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-15 16:25:13 +0100

    Connect Wallet component with redux

--- __tests__/Wallet.test.js
+++ __tests__/Wallet.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Enzyme from 'enzyme'
 import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
-import Wallet from '../src/components/Wallet';
+import { Wallet } from '../src/components/Wallet';
 
 Enzyme.configure({ adapter: new Adapter() })
 
