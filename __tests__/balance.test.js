69db13c6ff3e720b949850ee801c4eb643df8cfa
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-05-15 14:30:13 +0100

    Add actions file and balance.js and constants

--- __tests__/balance.test.js
+++ __tests__/balance.test.js
@@ -1,8 +1,8 @@
-import * as constants from '../src/constants';
-import * as actions from '../src/balance';
+import * as constants from '../src/actions/constants';
+import * as actions from '../src/actions/balance';
 
 it('creates an action to set the balance', () => {
   const balance = 0;
   const expectedAction = { type: constants.SET_BALANCE, balance };
-  expect(actions.setbalance(balance)).toEqual(expectedAction);
+  expect(actions.setBalance(balance)).toEqual(expectedAction);
 });
